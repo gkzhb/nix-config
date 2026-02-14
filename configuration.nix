1df0da4021205e7dd1ed4348d460be72e7fdbef8
gkzhb <gkzhb98@gmail.com>
2026-02-14 23:25:45 +0800

    feat: add syncthing service

--- configuration.nix
+++ configuration.nix
@@ -45,38 +45,9 @@
       "psql/admin_user" = { };
     };
   };
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
-  # Configure keymap in X11
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
-
-  # Enable sound.
-  # services.pulseaudio.enable = true;
-  # OR
-  # services.pipewire = {
-  #   enable = true;
-  #   pulse.enable = true;
-  # };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
+  i18n.defaultLocale = "en_US.UTF-8";
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zhb = {
@@ -136,53 +107,51 @@
     nil
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh = {
-    enable = true;
-    settings = {
-      PermitRootLogin = "no";
-      PasswordAuthentication = false;
-      KbdInteractiveAuthentication = false;
+  services = {
+    # Enable the OpenSSH daemon.
+    openssh = {
+      enable = true;
+      settings = {
+        PermitRootLogin = "no";
+        PasswordAuthentication = false;
+        KbdInteractiveAuthentication = false;
+      };
     };
-  };
-
-  services.frp = {
-    enable = true;
-    role = "client";
-    settings = {
-      serverAddr = "gkzhb.top";
-      serverPort = 7000;
-      auth.token = "gkzhb-frp";
-      proxies = [
-        {
-          name = "ssh";
-          type = "tcp";
-          localPort = 22;
-          remotePort = 4424;
-        }
-      ];
+    frp = {
+      enable = true;
+      role = "client";
+      settings = {
+        serverAddr = "gkzhb.top";
+        serverPort = 7000;
+        auth.token = "gkzhb-frp";
+        proxies = [
+          {
+            name = "ssh";
+            type = "tcp";
+            localPort = 22;
+            remotePort = 4424;
+          }
+        ];
+      };
     };
-  };
-
-  services.tailscale = {
-    enable = true;
-    openFirewall = true;
-    extraDaemonFlags = [ "--no-logs-no-support" ];
-  };
-
-  services.xray = {
-    enable = true;
-    settingsFile = "/etc/xray/config.json";
+    tailscale = {
+      enable = true;
+      openFirewall = true;
+      extraDaemonFlags = [ "--no-logs-no-support" ];
+    };
+    xray = {
+      enable = true;
+      settingsFile = "/etc/xray/config.json";
+    };
+    syncthing = {
+      enable = true;
+      # group = "mygroupname";
+      user = "zhb";
+      dataDir = "/home/zhb/syncthing"; # Default folder for new synced folders
+      configDir = "/home/zhb/.config/syncthing"; # Folder for Syncthing's settings and keys
+      guiAddress = "0.0.0.0:8384";
+    };
   };
 
   # Open ports in the firewall.
@@ -198,7 +167,7 @@
       80 # HTTP
       443 # HTTPS
     ];
-    # Allow LAN and Tailscale sources full access
+    # Allow Tailscale sources full access
     trustedInterfaces = [ "tailscale0" ];
     # Allow LAN traffic (common private IP ranges)
     extraCommands = ''
@@ -211,11 +180,6 @@
       iptables -D nixos-fw -s 100.64.0.0/8 -j nixos-fw-accept 2>/dev/null || true
     '';
   };
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
